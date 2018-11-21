67e98f55337e1be6de20fe715fab3979ef474b42
Sean Chavez <realseanchavez@gmail.com>
2018-11-20 22:47:07 -0800

    Crete database; test

---
+++ database.js
@@ -0,0 +1,29 @@
+const Sequelize = require('sequelize')
+const epilogue = requ
+ire('epilogue')
+
+const database = new Sequelize({
+  dialect: 'sqlite',
+  storage: './test.sqlite',
+  operatorsAliases: false,
+})
+
+const Part = database.define('parts', {
+  partNumber: Sequelize.STRING,
+  modelNumber: Sequelize.STRING,
+
+  name: Sequelize.STRING,
+  description: Sequelize.TEXT,
+})
+
+const initializeDatabase = async app => {
+  epilogue.initialize({ app, sequelize: database })
+
+  epilogue.resource({
+    model: Part,
+    endpoints: ['/parts', '/parts/:id'],
+  })
+  await database.sync()
+}
+
+module.exports = initializeDatabase
