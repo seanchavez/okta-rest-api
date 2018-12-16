3297c02f5c5a595369069887ced86ea509223036
Sean Chavez <realseanchavez@gmail.com>
2018-12-15 20:22:22 -0800

    Test protection of app

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const { promisify } = require('util')
-const initializeDatabase = require('./database')
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const { promisify } = require('util');
 
-const app = express()
-app.use(bodyParser.json())
+const authMiddleware = require('./auth');
+const initializeDatabase = require('./database');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(authMiddleware);
 
 const startServer = async () => {
-  await initializeDatabase(app)
-  const port = process.env.SERVER_PORT || 3000
-  await promisify(app.listen).bind(app)(port)
-  console.log(`Listening on port ${port}`)
-}
+  await initializeDatabase(app);
+  const port = process.env.SERVER_PORT || 3000;
+  await promisify(app.listen).bind(app)(port);
+  console.log(`Listening on port ${port}`);
+};
 
-startServer()
+startServer();
